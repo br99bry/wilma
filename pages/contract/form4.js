68e3b85aec905eda71fc19e025346f4f8b386b0a
Jonathan <jonathan.olmedo.p@hotmail.com>
2022-12-08 11:22:32 -0600

    actulizacion

--- pages/contract/form4.js
+++ pages/contract/form4.js
@@ -96,14 +96,6 @@ const ContractForm3 = () => {
                           value=""
                         />
                         <label className="flex items-center w-full m-1 px-2 py-2 text-xl font-medium bg-white border-2 border-v3-super-gray-400 shadow-sm cursor-pointer rounded-xl hover:bg-gray-100 p-2 font-medium">
-                          <input
-                            className="form-radio mr-2 border-2 border-v3-super-gray-400 text-v2-super-pink-500 ring-current ring-offset-current focus:ring-current "
-                            id="gender_radio_input_gastos_yes"
-                            name="life_policy_application[data][ingresos]"
-                            required=""
-                            type="text"
-                            disabled
-                          />
                         </label>
                       </div>
                     </div>
@@ -120,14 +112,6 @@ const ContractForm3 = () => {
                           value=""
                         />
                         <label className="flex items-center w-full m-1 px-2 py-2 text-xl font-medium bg-white border-2 border-v3-super-gray-400 shadow-sm cursor-pointer rounded-xl hover:bg-gray-100 p-2 font-medium">
-                          <input
-                            className="form-radio mr-2 border-2 border-v3-super-gray-400 text-v2-super-pink-500 ring-current ring-offset-current focus:ring-current "
-                            id="gender_radio_input_gastos_yes"
-                            name="life_policy_application[data][ingresos]"
-                            required=""
-                            type="text"
-                            disabled
-                          />
                         </label>
                       </div>
                     </div>
@@ -144,14 +128,6 @@ const ContractForm3 = () => {
                           value=""
                         />
                         <label className="flex items-center w-full m-1 px-2 py-2 text-xl font-medium bg-white border-2 border-v3-super-gray-400 shadow-sm cursor-pointer rounded-xl hover:bg-gray-100 p-2 font-medium">
-                          <input
-                            className="form-radio mr-2 border-2 border-v3-super-gray-400 text-v2-super-pink-500 ring-current ring-offset-current focus:ring-current "
-                            id="gender_radio_input_gastos_yes"
-                            name="life_policy_application[data][ingresos]"
-                            required=""
-                            type="text"
-                            disabled
-                          />
                         </label>
                       </div>
                     </div>
