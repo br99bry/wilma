114f6241b1e58b516c5b180ec3b54b19c013358e
Jonathan <jonathan.olmedo.p@hotmail.com>
2022-12-08 11:27:39 -0600

    border

--- pages/contract/form4.js
+++ pages/contract/form4.js
@@ -85,10 +85,10 @@ const ContractForm3 = () => {
                   </div>
                   <fieldset className="space-y-6">
                     <div className="w-full radio-box" id="ingresos_input_parent">
+                      <div className="flex items-center justify-between -mx-1 w-full">
                       <legend className="block text-lg leading-relaxed font-semibold ">
                       Ciudad: 
                       </legend>
-                      <div className="flex items-center justify-between -mx-1 w-full">
                         <input
                           id="life_policy_application_data_gender"
                           name="ingresos_facturacion"
