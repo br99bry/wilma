27b79647e6d34f66e933a6834adb28765ee197c7
Jonathan <jonathan.olmedo.p@hotmail.com>
2022-12-08 11:42:13 -0600

    width

--- pages/contract/form4.js
+++ pages/contract/form4.js
@@ -141,7 +141,7 @@ const ContractForm3 = () => {
                           type="hidden"
                           value=""
                         />
-                        <label className="flex items-center m-1 px-2 py-2 text-xl font-medium bg-white border-2 border-v3-super-gray-400 shadow-sm rounded-xl hover:bg-gray-100 p-2 font-medium width-inherit">
+                        <label className="flex items-center m-1 px-2 py-2 text-xl font-medium bg-white border-2 border-v3-super-gray-400 shadow-sm rounded-xl hover:bg-gray-100 p-2 font-medium">
                           <input
                             className="mr-2 border-v3-super-gray-400 text-v2-super-pink-500 ring-current ring-offset-current focus:ring-current background-none"
                             id="gender_radio_input_gastos_yes"
